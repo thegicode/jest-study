d02c9a68c2e9d807984ee67136d7f16b3c8c6f86
thegicode <thegi.code@gmail.com>
2021-07-29 16:27:29 +0900

    Move files

--- jest/src/mock2.test.js
+++ jest/src/mock2.test.js
@@ -4,5 +4,9 @@ console.log(myMock());
 
 myMock.mockReturnValueOnce(10).mockReturnValueOnce('x').mockReturnValue(true);
 
-console.log(myMock(), myMock(), myMock(), myMock());
-// > 10, 'x', true, true
+test('test ...', () => {
+
+    console.log(myMock(), myMock(), myMock(), myMock());
+    // > 10, 'x', true, true
+    
+});
